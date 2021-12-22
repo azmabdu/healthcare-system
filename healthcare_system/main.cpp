0ceed604f1100e0e30e1a34c2d313f5bec9cbd34
azmabdu <abduvohidovazim885@gmial.com>
2021-12-22 16:14:39 +0500

    Initial Commit

---
+++ healthcare_system/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  healthcare_system
+//
+//  Created by Azim on 12/22/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
